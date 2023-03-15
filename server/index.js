064b8365d8841db4efa577e507e9aa0923cdb9a8
anakinsonone <akkhi.sonone@gmail.com>
2023-03-16 01:32:23 +0530

    connect app to mongodb using mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
-
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("../server/schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server is running on : ${port}`));
